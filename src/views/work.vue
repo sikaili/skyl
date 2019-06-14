56a5acab0960aa60bfd075d90c66059b795bb46c
sikai <skyl@me.com>
2019-06-14 19:15:39 +0200

    2nd component doesnt work

--- src/views/work.vue
+++ src/views/work.vue
@@ -1,5 +1,8 @@
 <template>
-  <Menu :works="arrr" />
+  <div class="db">
+    <Menu :works="arrr" mName="Applis" />
+    <Menu :works="arr1" mName="Songs" />
+  </div>
 </template>
 
 <script>
@@ -12,6 +15,43 @@ export default {
   data() {
     return {
       arrr: [
+        {
+          name: "Eyes",
+          link: "http://eyes.skyl.fr",
+          img: require("@/assets/eyes.png"),
+          id: 0,
+          show: false
+        },
+        {
+          name: "Unsustainable",
+          link: "http://unsustainable.skyl.fr",
+          img: require("@/assets/eyes.png"),
+          id: 1,
+          show: false
+        },
+        {
+          name: "Where's Jane",
+          link: "http://unsustainable.skyl.fr",
+          img: require("@/assets/eyes.png"),
+          id: 2,
+          show: false
+        },
+        {
+          name: "Noisy Lines",
+          link: "http://unsustainable.skyl.fr",
+          img: require("@/assets/eyes.png"),
+          id: 3,
+          show: false
+        },
+        {
+          name: "Forces",
+          link: "http://forces.skyl.fr",
+          img: require("@/assets/eyes.png"),
+          id: 4,
+          show: false
+        }
+      ],
+      arr1: [
         {
           name: "Eyes",
           link: "http://eyes.skyl.fr",
