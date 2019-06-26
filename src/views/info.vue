be56a64e7321cbd72e70c7c6b69828a8adb9b120
sikai <skyl@me.com>
2019-06-27 00:57:33 +0200

    modif-play

--- src/views/info.vue
+++ src/views/info.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="f3 tc">
-    <a
+  <div class="flex flex-column items-start justify-end pb5 f3 tc">
+    <!-- <a
       @click="back"
       href="#0"
-      class="f5 no-underline white bg-black-40 bg-animate hover-bg-black hover-white inline-flex items-center pa3 border-box mr1"
+      class="self-center self-center f5 no-underline white bg-black-40 bg-animate hover-bg-black hover-white inline-flex items-center pa3 border-box mr1"
     >
       <svg
         class="w1"
@@ -15,9 +15,11 @@
         <path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
       </svg>
       <span class="pl1">Return</span>
-    </a>
+    </a> -->
     <br />
-    <p class="tc white bg-black-40">mail:skyl@me.com</p>
+    <p class="tc f6 white bg-black-40">info</p>
+    <p class="tc f6 white bg-black-40">mail: skyl@me.com</p>
+    <p class="tc f6 white bg-black-40">link: skyl.fr</p>
   </div>
 </template>
 
