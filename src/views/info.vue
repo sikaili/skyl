81b4c68d7628b41926a0c5cd4cdd9d0399d7d757
sikai <skyl@me.com>
2019-06-27 16:29:33 +0200

    now info is fixed so mouse action is not ignored

--- src/views/info.vue
+++ src/views/info.vue
@@ -1,28 +1,14 @@
 <template>
-  <div class="flex flex-column items-start justify-end pb5 f3 tc">
-    <!-- <a
-      @click="back"
-      href="#0"
-      class="self-center self-center f5 no-underline white bg-black-40 bg-animate hover-bg-black hover-white inline-flex items-center pa3 border-box mr1"
-    >
-      <svg
-        class="w1"
-        data-icon="chevronLeft"
-        viewBox="0 0 32 32"
-        style="fill:currentcolor"
-      >
-        <title>chevronLeft icon</title>
-        <path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
-      </svg>
-      <span class="pl1">Return</span>
-    </a> -->
+  <div class="fixed f3 tc">
     <br />
-    <p class="tc f6 white bg-black-40">
-      info_
-      <i class="fr mr2 icon ion-logo-twitch"></i>
-    </p>
-    <p class="tc f6 white bg-black-40">mail: skyl@me.com</p>
-    <p class="tc f6 white bg-black-40">link: skyl.fr</p>
+    <div class="f6 tl" style="min-height:5rem;">
+      <p class="white bg-black-40">
+        info
+        <i class="mh2 icon ion-logo-twitch"></i>
+      </p>
+      <p class="white bg-black-40">mail: skyl@me.com</p>
+      <p class="white bg-black-40">link: skyl.fr</p>
+    </div>
   </div>
 </template>
 
