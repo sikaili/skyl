a5787b9a9b1312577e3d538d919dab848963d82d
sikai <skyl@icloud.com>
2019-07-06 10:46:34 +0200

    add_new_drawings

--- src/views/draw.vue
+++ src/views/draw.vue
@@ -37,25 +37,53 @@ export default {
           link: require("@/assets/drawings/4.jpg")
         },
         {
+          name: "down",
+          link: require("@/assets/drawings/8.jpg")
+        },
+        {
           name: "up",
           link: require("@/assets/drawings/9.jpg")
         },
         {
-          name: "down",
-          link: require("@/assets/drawings/8.jpg")
+          name: "parts 01",
+          link: require("@/assets/drawings/parts1.jpg")
         },
         {
           name: "no escape",
           link: require("@/assets/drawings/7.jpg")
         },
         {
+          name: "parts 02",
+          link: require("@/assets/drawings/parts2.jpg")
+        },
+        {
           name: "the witch",
           link: require("@/assets/drawings/10.jpg")
         },
         {
+          name: "green",
+          link: require("@/assets/drawings/lv1.jpg")
+        },
+        {
           name: "the witch 02",
           link: require("@/assets/drawings/11.jpg")
+        },
+        {
+          name: "green 02",
+          link: require("@/assets/drawings/lv2.jpg")
+        },
+        {
+          name: "cell",
+          link: require("@/assets/drawings/cell.jpg")
         }
+        // {
+        //   name: "cell",
+        //   link: require("@/assets/drawings/up.jpg")
+        // },
+        // {
+        //   name: "cell",
+        //   link: require("@/assets/drawings/up1.jpg")
+        // }
       ]
     };
   }
