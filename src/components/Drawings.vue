d384a950bbcfd1e95bac54dbabcce753bf349e84
sikai <skyl@icloud.com>
2019-07-06 12:22:16 +0200

    css

--- src/components/Drawings.vue
+++ src/components/Drawings.vue
@@ -3,59 +3,57 @@
     <VueGallery :images="images" :index="index" @close="index = null" />
     <br />
 
-    <div @click="grid = !grid" class="mt3 fixed f2" style="z-index:1000;">
+    <div @click="grid = !grid" class="mt4 fixed f2" style="z-index:1000;">
       <span
         class="pv2 ph3 no-underline white bg-black-50 bg-animate hover-bg-white hover-black inline-flex items-center border-box"
       >
         <i v-if="grid == false" class="icon ion-md-apps"></i>
-        <i v-else class="icon ion-md-list"></i>
+        <i v-else class="icon ion-md-menu"></i>
       </span>
     </div>
-    <transition name="slide-fade1">
+    <div
+      v-if="grid == true"
+      class="mt3 center bt bb b--black-20 flex flex-wrap items-start"
+      :class="$mq == `sm` ? `w-100` : `w-80`"
+    >
       <div
-        v-if="grid == true"
-        class="center bt bb b--black-20 flex flex-wrap items-start"
-        :class="$mq == `sm` ? `w-100` : `w-80`"
+        v-for="(drawing, imgIndex) in drawings"
+        :key="imgIndex"
+        class="w-third white-50"
       >
-        <div
-          v-for="(drawing, imgIndex) in drawings"
-          :key="imgIndex"
-          class="w-third white-50"
-        >
-          <a @click="index = imgIndex" class="mt3 db link tc o-90">
-            <img
-              :src="drawing.link"
-              :alt="drawing.name"
-              class="db dim bw0 ba bw5 black-50 border-box"
-            />
-            <dl class="mt2 f6 lh-copy">
-              <dt class="clip">Title</dt>
-              <dd class="ml0 black truncate w-100">{{ drawing.name }}</dd>
-            </dl>
-          </a>
-        </div>
+        <a @click="index = imgIndex" class="mt3 db link tc o-90">
+          <img
+            :src="drawing.link"
+            :alt="drawing.name"
+            class="db dim bw0 ba bw5 black-50 border-box"
+          />
+          <dl class="mt2 f6 lh-copy">
+            <dt class="clip">Title</dt>
+            <dd class="ml0 black truncate w-100">{{ drawing.name }}</dd>
+          </dl>
+        </a>
       </div>
-      <div v-else class="bt bb b--black-20 cl">
-        <div
-          v-for="(drawing, imgIndex) in drawings"
-          :key="imgIndex"
-          class="center w-30 pa3 cl"
-          :class="$mq == `sm` ? `w-100` : `w-40`"
-        >
-          <a @click="index = imgIndex" class="mt3 db link tc o-90">
-            <img
-              :src="drawing.link"
-              alt="Frank Ocean Blonde Album Cover"
-              class="w-100 db dim ba bw5 white-50 border-box"
-            />
-            <dl class="mt2 f6 lh-copy">
-              <dt class="clip">Title</dt>
-              <dd class="ml0 black truncate w-100">{{ drawing.name }}</dd>
-            </dl>
-          </a>
-        </div>
+    </div>
+    <div v-else class="bt bb b--black-20 cl">
+      <div
+        v-for="(drawing, imgIndex) in drawings"
+        :key="imgIndex"
+        class="center w-30 pa3 cl"
+        :class="$mq == `sm` ? `w-100` : `w-40`"
+      >
+        <a @click="index = imgIndex" class="mt3 db link tc o-90">
+          <img
+            :src="drawing.link"
+            alt="Frank Ocean Blonde Album Cover"
+            class="w-100 db dim ba bw5 white-50 border-box"
+          />
+          <dl class="mt2 f6 lh-copy">
+            <dt class="clip">Title</dt>
+            <dd class="ml0 black truncate w-100">{{ drawing.name }}</dd>
+          </dl>
+        </a>
       </div>
-    </transition>
+    </div>
   </div>
 </template>
 
