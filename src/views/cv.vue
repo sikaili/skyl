47d3a8273cab37598d8807b97c1d96b0417a5412
sikai <skyl@icloud.com>
2019-08-09 16:19:59 +0200

    add-cart

--- src/views/cv.vue
+++ src/views/cv.vue
@@ -34,11 +34,15 @@
         <tbody class="lh-copy">
           <tr>
             <td class="pv3 pr3 bb b--black-20">Langages</td>
-            <td class="b pv3 pr3 bb b--black-20">Javascript, CSS, HTML, PHP/Symfony</td>
+            <td class="b pv3 pr3 bb b--black-20">
+              Javascript, CSS, HTML, PHP/Symfony
+            </td>
           </tr>
           <tr>
             <td class="pv3 pr3 bb b--black-20">Librairies</td>
-            <td class="b pv3 pr3 bb b--black-20">Vue.js, P5.js, jQuery, Bootstrap, Tachyons, Symfony</td>
+            <td class="b pv3 pr3 bb b--black-20">
+              Vue.js, P5.js, jQuery, Bootstrap, Tachyons, Symfony
+            </td>
           </tr>
           <tr>
             <td class="pv3 pr3 bb b--black-20">Outils & Plug-ins</td>
@@ -60,12 +64,25 @@
         d’explorer de nouvelles solutions pour un futur désirable. Co-fondateur
         d’une série de jeux-videos parlant de la pollution plastique.
       </h3>
+      <div class="w-100">
+        <div class="w-40 card pa3 ba bw0 border-box">
+          <img :src="require(`@/assets/plastic-arcade.png`)" />
+          <p>Kobini: "Plastic Arcade", quand le jeu vidéo devient écolo</p>
+          <span
+            @click="goToPage(item.id)"
+            class="tc w4 f6 dim link ba bw2 ph2 pv1 mt3 dib black"
+            >Read more..</span
+          >
+        </div>
+      </div>
       <ul class="lh-copy">
         <li>
           Différentes séances de 'Proof of Concept', avec les experts du domaine
           et le grand publique
         </li>
-        <li>Prototypage des idées jeux-videos en Javascript (p5.js et Canvas API)</li>
+        <li>
+          Prototypage des idées jeux-videos en Javascript (p5.js et Canvas API)
+        </li>
         <ul>
           <li>Prototypage UI en utilisant Tachyons CSS et Bootstrap</li>
         </ul>
@@ -89,9 +106,9 @@
           </tr>
           <tr>
             <td class="pv3 pr3 bb b--black-20">Outils & Plug-ins</td>
-            <td
-              class="b pv3 pr3 bb b--black-20"
-            >Git, Gulp, Visual Studio, Arduino IDE, Processing IDE, Unity</td>
+            <td class="b pv3 pr3 bb b--black-20">
+              Git, Gulp, Visual Studio, Arduino IDE, Processing IDE, Unity
+            </td>
           </tr>
         </tbody>
       </table>
@@ -106,6 +123,11 @@ export default {};
 <style scoped>
 * {
   font-family: "Helvetica Neue", sans-serif;
+}
+.card:hover {
+  box-sizing: border-box;
+  border-width: 1px;
+  transition: border-width 0.15s;
 }
 ul li {
   padding: 0.5rem 0;
