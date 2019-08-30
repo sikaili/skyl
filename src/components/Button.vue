02fd160d15988062a6568cf29f04054ccebf245d
sikai <skyl@icloud.com>
2019-08-30 19:05:08 +0200

    button

---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<template functional>
+  <span
+    class="f5 no-underline white bg-black-40 bg-animate hover-bg-black hover-white inline-flex items-center pa3 border-box mr1"
+    :class="props.style"
+  >
+    <i class="icon ion-md-return-left"></i>
+
+    <span class="pl1"><slot></slot></span>
+  </span>
+</template>
