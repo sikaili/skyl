b7a63a328b459ad83d3983bd3e6e39210bd5f909
Sikai Li <sli-ext@lequipe.fr>
2019-09-20 17:36:44 +0200

    add vue/pwa

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import store from "./store";
 import router from "./router";
 import VueMq from "vue-mq";
+import './registerServiceWorker'
 
 Vue.use(VueMq, {
   breakpoints: {
