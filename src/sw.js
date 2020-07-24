77fd64b97b3d171d9da8e7e3fcce8507d23a44f4
Sikai Li <sli-ext@lequipe.fr>
2020-07-24 12:04:16 +0200

    changed:disable sw debug

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-workbox.setConfig({ debug: true });
+// workbox.setConfig({ debug: true });
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
