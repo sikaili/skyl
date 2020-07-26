500773e02d6986ffd90d96ca0716070d7a6af643
Sikai Li <sli-ext@lequipe.fr>
2020-07-26 19:47:26 +0200

    added:gtag

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import store from '@/store';
 import router from '@/router';
 
 import './registerServiceWorker';
-import VueCompositionAPI from '@vue/composition-api';
+// import VueCompositionAPI from '@vue/composition-api';
 
-Vue.use(VueCompositionAPI);
+// Vue.use(VueCompositionAPI);
 Vue.use(VueMq, {
   breakpoints: {
     sm: 675,
