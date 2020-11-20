17216e148e492499581110bf543f53fb4bf1cdf0
Sikai Li <sli-ext@lequipe.fr>
2020-11-20 21:21:57 +0100

    added:nodejs create sketch automation

---
+++ create.js
@@ -0,0 +1,69 @@
+const child = require('child_process');
+const fs = require('fs');
+const readline = require('readline');
+
+const SKETCHES_PATH = 'src/assets/JSON/sketches.json';
+const SKETCHE_JS_PATH = (name) => `src/projects/${name}/${name}.js`;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let projectName = '';
+rl.question('Choose a project name:', (name) => {
+  projectName = name;
+  rl.close();
+});
+
+rl.on('close', () => {
+  const rawdata = fs.readFileSync(SKETCHES_PATH);
+  const sketches = JSON.parse(rawdata);
+  sketches.push({
+    id: projectName,
+    type: 'sketch',
+  });
+  fs.writeFileSync(SKETCHES_PATH, JSON.stringify(sketches, null, '\t'));
+  child.execSync(`cd src/projects && mkdir ${projectName} && cd ${projectName} && touch ${projectName}.js && cd ../..`);
+  fs.writeFileSync(SKETCHE_JS_PATH(projectName), `import setListeners from '@/js/utlis/addEventListeners';
+  
+console.log('import ${projectName}');
+
+const sketch = (instance) => {
+  const sk = instance;
+
+  sk.start = () => {
+    sk.loop();
+  };
+
+  sk.stop = () => {
+    try {
+      sk.noLoop();
+      sk.remove();
+      console.log('${projectName} killed');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  sk.setup = () => {
+    sk.canvas = sk.createCanvas(sk.windowWidth, sk.windowHeight);
+    sk.background(255, 0, 0);
+    sk.mouseX = sk.width / 2;
+    sk.mouseY = sk.height / 2;
+  };
+
+  sk.draw = () => {
+  };
+
+  sk.windowResized = () => {
+    sk.resizeCanvas(sk.windowWidth, sk.windowHeight);
+  };
+
+  setListeners(sk);
+};
+
+export default sketch;
+`);
+  console.log(`${projectName} is created`);
+  process.exit(0);
+});
